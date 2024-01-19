07557ce12467a6d77ed454175f710c4666a8e01d
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2024-01-19 15:11:44 +0300

    fix: delete annotations

--- src/main/java/com/blogify/blogapi/model/Comment.java
+++ src/main/java/com/blogify/blogapi/model/Comment.java
@@ -1,12 +1,11 @@
 package com.blogify.blogapi.model;
 
+import java.io.Serializable;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -22,21 +21,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class Comment {
+public class Comment implements Serializable {
     @Id
     private String id;
 
-    @Column(name = "content")
     private String content;
 
-    @Column(name = "creation_date_time")
-    private Instant creationDateTime;
+    private Instant creationDatetime;
 
     /* @ManyToOne
     @JoinColumn(name = "post_id")
     private Post post;
     // TODO: Add relation with post, waiting for the completion of the Post entity */
 
-    @OneToMany(mappedBy = "comment")
-    private List<CommentReaction> commentReaction;
+    @OneToMany
+    @JoinColumn(name = "comment_id")
+    private List<CommentReaction> reactions;
 }
