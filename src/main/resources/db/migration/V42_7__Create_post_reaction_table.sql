78b3654a4d458e27697436bd90f80b29a1a3c810
Mirado <hei.mirado@gmail.com>
2024-01-19 19:08:42 +0300

    change reaction type varchar to enum

--- src/main/resources/db/migration/V42_7__Create_post_reaction_table.sql
+++ src/main/resources/db/migration/V42_7__Create_post_reaction_table.sql
@@ -1,6 +1,8 @@
+create type "reaction_type" as enum ('LIKE', 'DISLIKE');
+
 create table if not exists post_reaction
 (
     id VARCHAR(255) PRIMARY KEY,
     id_post VARCHAR(255) REFERENCES post(id),
-    type VARCHAR(255)
+    type reaction_type
     );
