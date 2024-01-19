d468ad516a7f1f740c949d7b0b80b496309e23e8
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2024-01-19 15:12:50 +0300

    delete attributs

--- src/main/java/com/blogify/blogapi/model/CommentReaction.java
+++ src/main/java/com/blogify/blogapi/model/CommentReaction.java
@@ -1,39 +1,17 @@
 package com.blogify.blogapi.model;
 
-import java.util.UUID;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "comment_reaction")
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
-public class CommentReaction {
-    @Id
-    private String id;
-
-    /* @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-    // TODO: Add relation with User, waiting for the completion of the User entity */
-
-    @ManyToOne
-    @JoinColumn(name = "comment_id")
-    private Comment comment;
-
-    @Column(name = "is_like")
-    private boolean isLike;
-}
+@EqualsAndHashCode(callSuper = false)
+public class CommentReaction extends Reaction{}
 
