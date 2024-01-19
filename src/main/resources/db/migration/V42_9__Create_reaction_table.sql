18fa0c0467ffde667d0175638972a7c81238ec31
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2024-01-19 15:09:16 +0300

    refactor: organisation file

--- src/main/resources/db/migration/V42_9__Create_reaction_table.sql
+++ src/main/resources/db/migration/V42_9__Create_reaction_table.sql
@@ -1,6 +1,7 @@
 create table if not exists comment_reaction
 (
     id varchar primary key default uuid_generate_v4(),
+    user_id varchar REFERENCES user(id),
     comment_id varchar REFERENCES comment(id),
     is_like BOOLEAN
 );
