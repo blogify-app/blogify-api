4273f2c70ea77243c95b60e30f9e80b7f827a62c
NyH1409 <hei.nyhasina@gmail.com>
2024-01-20 11:41:49 +0300

    feat: auth provider

---
+++ src/main/java/com/blogify/blogapi/endpoint/security/AuthProvider.java
@@ -0,0 +1,39 @@
+package com.blogify.blogapi.endpoint.security;
+
+import com.google.auth.Credentials;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthProvider {
+  private static final String BEARER_PREFIX = "Bearer ";
+  private final SecurityContext context;
+
+  public AuthProvider() {
+    context = SecurityContextHolder.getContext();
+  }
+
+  public User getUser() {
+    Object authUser = context.getAuthentication().getPrincipal();
+    if (authUser instanceof User) {
+      return (User) authUser;
+    }
+    return null;
+  }
+
+  public Credentials getCredentials() {
+    return (Credentials) context.getAuthentication().getCredentials();
+  }
+
+
+  public String getBearer(HttpServletRequest req) {
+    String authorization = req.getHeader("Authorization");
+    if (authorization.startsWith(BEARER_PREFIX)) {
+      return authorization.substring(7);
+    }
+    return null;
+  }
+}
