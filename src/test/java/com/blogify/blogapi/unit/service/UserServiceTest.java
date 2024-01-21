019aaa4222daf75447f0521f96be9c0adb8393a7
NyH1409 <hei.nyhasina@gmail.com>
2024-01-21 10:05:06 +0300

    chore: unit test for user service

---
+++ src/test/java/com/blogify/blogapi/unit/service/UserServiceTest.java
@@ -0,0 +1,38 @@
+package com.blogify.blogapi.unit.service;
+
+import com.blogify.blogapi.conf.FacadeIT;
+import com.blogify.blogapi.repository.model.User;
+import com.blogify.blogapi.service.UserService;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.blogify.blogapi.utils.TestUtils.ignoreIdAnCreationDatetime;
+import static com.blogify.blogapi.utils.TestUtils.user;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserServiceTest extends FacadeIT {
+  @Autowired
+  UserService subject;
+
+
+  @Test
+  void create_user_ok(){
+    User actual = subject.save(user());
+
+    assertEquals(ignoreIdAnCreationDatetime(user()), ignoreIdAnCreationDatetime(actual));
+  }
+
+  @Test
+  void read_all_user(){
+    User created = subject.save(user());
+    List<User> actuals = subject.findAll();
+    var actual = actuals.get(0);
+
+    assertEquals(1, actuals.size());
+    assertEquals(created.getId(), actual.getId());
+    assertEquals(created.getFirstname(), actual.getFirstname());
+    assertEquals(created.getLastname(), actual.getLastname());
+    assertEquals(created.getMail(), actual.getMail());
+  }
+}
