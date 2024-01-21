4e9cee3d86c21c5bfbaedcee9b91e52e7be6267a
VAGNO Ny Hasina Marolahy <98644255+NyH1409@users.noreply.github.com>
2024-01-21 13:57:12 +0300

    feat: firebase bearer authentication
    * chore: add required deps
    
    * feat: auth filter
    
    * feat: auth provider
    
    * chore: security conf
    
    * chore: use custom exception
    
    * feat: setup protected endpoints
    
    * chore: authentication with firebase
    
    * chore: log authenticated user
    
    * todo: get authenticated user
    
    * feat: create firebase user
    
    * chore: configure cors
    
    * chore: test firebase service
    
    * chore: format code
    
    * chore: unit test for user service
    
    * refactor: no need to set a bidirectionnal relationship
    
    * feat: signup with firebase
    
    * chore: unit test for category service

--- src/main/java/com/blogify/blogapi/endpoint/rest/controller/CategoryController.java
+++ src/main/java/com/blogify/blogapi/endpoint/rest/controller/CategoryController.java
@@ -1,6 +1,6 @@
 package com.blogify.blogapi.endpoint.rest.controller;
 
-import com.blogify.blogapi.model.Category;
+import com.blogify.blogapi.repository.model.Category;
 import com.blogify.blogapi.service.CategoryService;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -13,7 +13,7 @@ public class CategoryController {
 
   private final CategoryService categoryService;
 
-  //TODO: Should be mapped when JavaClient is correctly set
+  // TODO: Should be mapped when JavaClient is correctly set
   @GetMapping(value = "/categories")
   public List<Category> send_emails() {
     return categoryService.findAll();
---
+++ src/test/java/com/blogify/blogapi/utils/TestUtils.java
@@ -0,0 +1,49 @@
+package com.blogify.blogapi.utils;
+
+import static com.blogify.blogapi.model.enums.Role.CLIENT;
+import static com.blogify.blogapi.model.enums.Sex.M;
+import static java.util.UUID.randomUUID;
+
+import com.blogify.blogapi.repository.model.Category;
+import com.blogify.blogapi.repository.model.User;
+import java.time.Instant;
+import java.util.List;
+
+public class TestUtils {
+  public static User user() {
+    String userId = randomUUID().toString();
+
+    return User.builder()
+        .id(userId)
+        .mail("dummy@gmail.com")
+        .firstname("John")
+        .role(CLIENT)
+        .userCategories(List.of())
+        .sex(M)
+        .lastname("Doe")
+        .build();
+  }
+
+  public static User ignoreIdAnCreationDatetime(User user) {
+    return user.toBuilder().id(null).creationDatetime(null).build();
+  }
+
+  public static List<User> ignoreIdsAndCreationDatetime(List<User> users) {
+    return users.stream()
+        .peek(
+            user -> {
+              user.setId(null);
+              user.setCreationDatetime(null);
+            })
+        .toList();
+  }
+
+  public static Category category() {
+    return Category.builder()
+        .id(randomUUID().toString())
+        .name("machine learning")
+        .creationDatetime(Instant.now())
+        .userCategories(List.of())
+        .build();
+  }
+}
