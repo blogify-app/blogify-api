d6849e755c84c429a4d16ee5e09f03aef94ac9e1
NyH1409 <hei.nyhasina@gmail.com>
2024-01-21 14:47:11 +0300

    feat: sign up with firebase

---
+++ src/main/java/com/blogify/blogapi/endpoint/mapper/UserMapper.java
@@ -0,0 +1,43 @@
+package com.blogify.blogapi.endpoint.mapper;
+
+import com.blogify.blogapi.endpoint.rest.model.Sex;
+import com.blogify.blogapi.endpoint.rest.model.SignUp;
+import com.blogify.blogapi.endpoint.rest.model.User;
+import java.time.Instant;
+import org.springframework.stereotype.Component;
+
+import static java.util.UUID.randomUUID;
+
+@Component
+public class UserMapper {
+
+  public User toRest(com.blogify.blogapi.repository.model.User domain) {
+    return new User()
+        .id(domain.getId())
+        .firstName(domain.getFirstname())
+        .lastName(domain.getFirstname())
+        .email(domain.getMail())
+        .birthDate(domain.getBirthdate())
+        .sex(toRestSex(domain.getSex()));
+  }
+
+
+  public com.blogify.blogapi.repository.model.User toDomain(SignUp rest){
+    return com.blogify.blogapi.repository.model.User.builder()
+        .id(randomUUID().toString())
+        .firstname(rest.getFirstName())
+        .lastname(rest.getLastName())
+        .birthdate(rest.getBirthDate())
+        .creationDatetime(Instant.now())
+        .mail(rest.getEmail())
+        .build();
+  }
+
+  private Sex toRestSex(com.blogify.blogapi.model.enums.Sex sex){
+    return switch (sex){
+      case M -> Sex.M;
+      case F -> Sex.F;
+    };
+  }
+
+}
--- src/main/java/com/blogify/blogapi/endpoint/rest/controller/SecurityController.java
+++ src/main/java/com/blogify/blogapi/endpoint/rest/controller/SecurityController.java
@@ -1,10 +1,11 @@
 package com.blogify.blogapi.endpoint.rest.controller;
 
-import com.blogify.blogapi.repository.model.User;
+import com.blogify.blogapi.endpoint.mapper.UserMapper;
+import com.blogify.blogapi.endpoint.rest.model.SignUp;
+import com.blogify.blogapi.endpoint.rest.model.User;
 import com.blogify.blogapi.service.UserService;
 import com.blogify.blogapi.service.firebase.FirebaseService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
   private final UserService userService;
   private final FirebaseService firebaseService;
+  private final UserMapper userMapper;
 
   @PostMapping("/signup")
-  public User signup(@RequestBody User toCreate) {
-    var user = firebaseService.createUser(toCreate, "");
-    return userService.save(user);
+  public User signup(@RequestBody SignUp toCreate) {
+    var domain = userMapper.toDomain(toCreate);
+    var user = firebaseService.createUser(domain, toCreate.getPassword());
+    return userMapper.toRest(userService.save(user));
   }
+  
 
-  @GetMapping("/auth")
-  public String auth() {
-    return "auth";
-  }
 }
