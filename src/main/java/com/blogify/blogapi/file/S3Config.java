e1cd5f63d3c5dbdae46f3b684f641ef9c1be2fff
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2024-01-26 22:20:07 +0300

    fix: s3 config & service

---
+++ src/main/java/com/blogify/blogapi/file/S3Config.java
@@ -0,0 +1,22 @@
+package com.blogify.blogapi.file;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+@Configuration
+public class S3Config {
+
+  @Value("${aws.region}")
+  private String awsS3Region;
+
+  @Bean
+  public S3Client amazonS3Client() {
+    return S3Client.builder()
+        .region(Region.of(awsS3Region))
+        .credentialsProvider(DefaultCredentialsProvider.create())
+        .build();
+  }
+}
--- src/main/java/com/blogify/blogapi/service/UserFileService.java
+++ src/main/java/com/blogify/blogapi/service/UserFileService.java
@@ -3,8 +3,8 @@ package com.blogify.blogapi.service;
 import com.blogify.blogapi.endpoint.rest.model.UserPicture;
 import com.blogify.blogapi.endpoint.rest.model.UserPictureType;
 import com.blogify.blogapi.file.BucketComponent;
+import com.blogify.blogapi.file.S3Service;
 import com.blogify.blogapi.repository.model.User;
-import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import lombok.AllArgsConstructor;
@@ -15,14 +15,14 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 public class UserFileService {
   private final UserService userService;
+  private final S3Service s3Service;
   private final BucketComponent bucketComponent;
 
   public UserPicture uploadUserPicture(String uid, UserPictureType type, MultipartFile file)
       throws IOException {
     User user = userService.findById(uid);
     String fileBucketKey = type == UserPictureType.PROFILE ? user.getPhotoUrl(): user.getProfileBannerUrl();
-    File file1 = (File) file;
-    bucketComponent.upload(file1, fileBucketKey);
+    s3Service.uploadObjectToS3Bucket(fileBucketKey,file.getBytes());
     String fileURL = String.valueOf(bucketComponent.presign(fileBucketKey, Duration.ofMinutes(2)));
     UserPicture userPicture = new UserPicture();
     userPicture.setUserId(uid);
