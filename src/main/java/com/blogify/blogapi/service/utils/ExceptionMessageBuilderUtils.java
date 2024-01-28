097bd07d4e4f663d6181bef1bdcc115306892780
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2024-01-28 17:59:15 +0300

    feat: add ExceptionMessageBuilderUtils

--- src/main/java/com/blogify/blogapi/service/utils/ExceptionMessageBuilderUtils.java
+++ src/main/java/com/blogify/blogapi/service/utils/ExceptionMessageBuilderUtils.java
@@ -1,2 +1,8 @@
-package com.blogify.blogapi.service.utils;public class ExceptionMessageBuilderUtils {
+package com.blogify.blogapi.service.utils;
+
+public class ExceptionMessageBuilderUtils {
+  public static String notFoundByIdMessageException(String resource, String id){
+    return "Resource of type "+resource+" identified by "+id+" not found";
+  }
+
 }
