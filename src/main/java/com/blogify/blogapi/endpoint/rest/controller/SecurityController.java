fafa83805a177365965e0e75bb5fbcfb290c19d4
VAGNO Ny Hasina Marolahy <98644255+NyH1409@users.noreply.github.com>
2024-01-30 18:05:46 +0300

    feat: signin user

--- src/main/java/com/blogify/blogapi/endpoint/rest/controller/SecurityController.java
+++ src/main/java/com/blogify/blogapi/endpoint/rest/controller/SecurityController.java
@@ -1,11 +1,14 @@
 package com.blogify.blogapi.endpoint.rest.controller;
 
 import com.blogify.blogapi.endpoint.mapper.UserMapper;
+import com.blogify.blogapi.endpoint.mapper.WhoamiMapper;
+import com.blogify.blogapi.endpoint.rest.model.AuthenticationPayload;
 import com.blogify.blogapi.endpoint.rest.model.SignUp;
 import com.blogify.blogapi.endpoint.rest.model.User;
+import com.blogify.blogapi.endpoint.rest.model.Whoami;
 import com.blogify.blogapi.model.enums.Role;
 import com.blogify.blogapi.service.UserService;
-import com.blogify.blogapi.service.firebase.FirebaseService;
+import com.blogify.blogapi.service.WhoamiService;
 import java.util.ArrayList;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,13 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class SecurityController {
   private final UserService userService;
-  private final FirebaseService firebaseService;
   private final UserMapper userMapper;
+  private final WhoamiService whoamiService;
+  private final WhoamiMapper mapper;
 
   @PostMapping("/signup")
   public User signup(@RequestBody SignUp toCreate) {
     var domain =
         userMapper.toDomain(toCreate, new ArrayList<>()).toBuilder().role(Role.CLIENT).build();
     return userMapper.toRest(userService.save(domain));
+  }
+
+  @PostMapping("/signin")
+  public Whoami signin(@RequestBody(required = false) AuthenticationPayload payload) {
+    var whoami = whoamiService.whoami();
+    return mapper.toRest(whoami);
   }
 }
