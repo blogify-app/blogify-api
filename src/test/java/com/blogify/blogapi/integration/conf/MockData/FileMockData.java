78c6e60c3c2db394d124242328717a811727444b
herilala <hei.herilala@gmail.com>
2024-02-03 10:51:39 +0300

    fix/ci

--- src/test/java/com/blogify/blogapi/integration/conf/MockData/FileMockData.java
+++ src/test/java/com/blogify/blogapi/integration/conf/MockData/FileMockData.java
@@ -7,41 +7,41 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileMockData {
-  private static byte[] readFileToByteArray(String filePath) {
-    try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-      //      fileInputStream.read(bytes);
-      return new byte[fileInputStream.available()];
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  static String jpgFilePath =
-      "/home/herilala/Bureau/HEI/Prog5/projet-group/Back/blogify-api/src/main/resources/file/testFile/test.jpg";
-
-  public static File jpgFile() {
-    File file = new File(jpgFilePath);
-    System.out.println(
-        "-----------------------------//////////////////////////////////////////////////-----------------------------------------------...00");
-    System.out.println(file);
-    return file;
-  }
-
-  public static byte[] jpgFileContent() {
-    File file = new File(jpgFilePath);
-    System.out.println(file);
-    return readFileToByteArray(jpgFilePath);
-  }
-  ;
-
-  public static MultipartFile jpgMultipartFile() {
-    return new MockMultipartFile(
-        "test", // File name
-        "test.png", // Origin file name
-        "image/png", // MIME Type
-        jpgFileContent());
-  }
-  ;
+//  private static byte[] readFileToByteArray(String filePath) {
+//    try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+//      //      fileInputStream.read(bytes);
+//      return new byte[fileInputStream.available()];
+//    } catch (IOException e) {
+//      throw new RuntimeException(e);
+//    }
+//  }
+//
+//  static String jpgFilePath =
+//      "/home/herilala/Bureau/HEI/Prog5/projet-group/Back/blogify-api/src/main/resources/file/testFile/test.jpg";
+//
+//  public static File jpgFile() {
+//    File file = new File(jpgFilePath);
+//    System.out.println(
+//        "-----------------------------//////////////////////////////////////////////////-----------------------------------------------...00");
+//    System.out.println(file);
+//    return file;
+//  }
+//
+//  public static byte[] jpgFileContent() {
+//    File file = new File(jpgFilePath);
+//    System.out.println(file);
+//    return readFileToByteArray(jpgFilePath);
+//  }
+//  ;
+//
+//  public static MultipartFile jpgMultipartFile() {
+//    return new MockMultipartFile(
+//        "test", // File name
+//        "test.png", // Origin file name
+//        "image/png", // MIME Type
+//        jpgFileContent());
+//  }
+//  ;
 
   //  static String pngFilePath = "file/testFile/test.png";
   //  static byte[] pngFileContent = readFileToByteArray(pngFilePath);
