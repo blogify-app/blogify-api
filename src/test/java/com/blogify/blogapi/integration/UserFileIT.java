16a1929cbace1e74e25be2b88ae4dc3060536bf7
mirado447 <103623110+mirado447@users.noreply.github.com>
2024-02-08 22:21:50 +0300

    Test/self macher user endpoint blog76 (#73)
    * Revert "fix: change securityconf (#70)"
    
    This reverts commit 41e427411484bf74da8c4c4dfd68b02a369bf517.
    
    * test:self macher for user endpoint
    
    ---------
    
    Co-authored-by: Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>

--- src/test/java/com/blogify/blogapi/integration/UserFileIT.java
+++ src/test/java/com/blogify/blogapi/integration/UserFileIT.java
@@ -1,13 +1,18 @@
 package com.blogify.blogapi.integration;
 
 import static com.blogify.blogapi.integration.conf.MockData.UserMockData.CLIENT1_ID;
+import static com.blogify.blogapi.integration.conf.MockData.UserMockData.CLIENT2_ID;
+import static com.blogify.blogapi.integration.conf.MockData.UserMockData.client1;
 import static com.blogify.blogapi.integration.conf.MockData.UserMockData.userPictureClient1Banner;
 import static com.blogify.blogapi.integration.conf.MockData.UserMockData.userPictureClient1Profile;
 import static com.blogify.blogapi.integration.conf.TestUtils.CLIENT1_TOKEN;
+import static com.blogify.blogapi.integration.conf.TestUtils.CLIENT2_TOKEN;
 import static com.blogify.blogapi.integration.conf.TestUtils.anAvailableRandomPort;
 import static com.blogify.blogapi.integration.conf.TestUtils.setUpFirebase;
 import static com.blogify.blogapi.integration.conf.TestUtils.setUpS3Service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 import com.blogify.blogapi.endpoint.rest.api.UserApi;
@@ -25,6 +30,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestComponent;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.io.File;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @TestComponent
@@ -56,6 +63,45 @@ public class UserFileIT {
     assertEquals(actualUserPictureProfile, userPictureClient1Profile());
   }
 
+  @Test
+  void other_client_update_picture_ko() throws ApiException{
+    ApiClient client2Client = anApiClient(CLIENT2_TOKEN);
+    UserApi api = new UserApi(client2Client);
+
+    ApiException exception1 =
+            assertThrows(
+                    ApiException.class,
+                    () -> api.putUserPicture(CLIENT1_ID, UserPictureType.PROFILE, new File("/home/test.jpg"))
+            );
+    ApiException exception2 =
+            assertThrows(
+                    ApiException.class,
+                    () -> api.putUserPicture(CLIENT1_ID, UserPictureType.BANNER, new File("/home/test.jpg"))
+            );
+    assertTrue(exception1.getMessage().contains("status\":403,\"error\":\"Forbidden"));
+    assertTrue(exception2.getMessage().contains("status\":403,\"error\":\"Forbidden"));
+
+  }
+
+  @Test
+  void other_client_delete_picture_ko() throws ApiException{
+    ApiClient client2Client = anApiClient(CLIENT2_TOKEN);
+    UserApi api = new UserApi(client2Client);
+
+    ApiException exception1 =
+            assertThrows(
+                    ApiException.class,
+                    () -> api.deleteUserPicture(CLIENT1_ID,UserPictureType.PROFILE)
+            );
+    ApiException exception2 =
+            assertThrows(
+                    ApiException.class,
+                    () -> api.deleteUserPicture(CLIENT1_ID,UserPictureType.BANNER)
+            );
+
+    assertTrue(exception1.getMessage().contains("status\":403,\"error\":\"Forbidden"));
+    assertTrue(exception2.getMessage().contains("status\":403,\"error\":\"Forbidden"));
+  }
   //  @Test
   //  void client_write_ok() throws ApiException {
   //    ApiClient client1Client = anApiClient(CLIENT1_TOKEN);
