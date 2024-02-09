451091ad4d93bf8d800b8a6099754a3f3d30d149
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2024-02-09 16:43:45 +0300

    refactor: remove unused code

--- src/main/java/com/blogify/blogapi/file/validator/ImageValidator.java
+++ src/main/java/com/blogify/blogapi/file/validator/ImageValidator.java
@@ -14,19 +14,8 @@ public class ImageValidator {
 
   public void accept(byte[] file) {
     if (file.length > FILE_MAX_SIZE_AS_MB * 1024 * 1024) { // 2MB in bytes
-      throw new BadRequestException("FIle size must be less than "+FILE_MAX_SIZE_AS_MB+"MB");
+      throw new BadRequestException("FIle size must be less than " + FILE_MAX_SIZE_AS_MB + "MB");
     }
     byteArrayTypeValidator.accept(file, "image");
   }
-
-  private byte[] hexStringToByteArray(String s) {
-    int len = s.length();
-    byte[] data = new byte[len / 2];
-    for (int i = 0; i < len; i += 2) {
-      data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-          + Character.digit(s.charAt(i+1), 16));
-    }
-    return data;
-  }
-
 }
